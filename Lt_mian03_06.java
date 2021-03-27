8c26a7ac84fcc111b2819d576c62e37904b48c42
xiaolong9420 <65012037+xiaolong9420@users.noreply.github.com>
2021-03-27 16:53:10 +0800

    默认的

---
+++ Lt_mian03_06.java
@@ -0,0 +1,32 @@
+package leetcode_exercise;
+
+public class Lt_mian03_06 {
+    public static void main(String[] args) {
+
+    }
+    class AnimalShelf {
+
+        public AnimalShelf() {
+
+        }
+
+        public void enqueue(int[] animal) {
+
+        }
+
+        public int[] dequeueAny() {
+
+            return new int[0];
+        }
+
+        public int[] dequeueDog() {
+
+            return new int[0];
+        }
+
+        public int[] dequeueCat() {
+
+            return new int[0];
+        }
+    }
+}
---
+++ Lt_mian16_02.java
@@ -0,0 +1,22 @@
+package leetcode_exercise;
+
+import java.util.HashMap;
+
+public class Lt_mian16_02 {
+    public static void main(String[] args) {
+
+    }
+    class WordsFrequency {
+        private HashMap<String,Integer> map;
+        public WordsFrequency(String[] book) {
+            map = new HashMap<>();
+            for (String str : book) {
+                map.put(str, map.getOrDefault(str, 0) + 1);
+            }
+        }
+
+        public int get(String word) {
+            return map.getOrDefault(word,0);
+        }
+    }
+}
