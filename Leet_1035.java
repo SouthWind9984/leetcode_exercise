e7d7d58e9fc0e1398b05ffda312adb8670c999fc
xiaolong9420 <65012037+SouthWind9984@users.noreply.github.com>
2021-05-21 23:17:40 +0800

    Leet刷题日记

---
+++ Leet_1035.java
@@ -0,0 +1,24 @@
+package leetcode_exercise;
+
+public class Leet_1035 {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        //转化为最长公共子序列问题
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
