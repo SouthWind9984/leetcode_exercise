3d3460cdb314b6997e73dcb235f73b11c011827a
xiaolong9420 <65012037+SouthWind9984@users.noreply.github.com>
2021-10-01 18:17:06 +0800

    Leet刷题日记

--- Leet_1436.java
+++ Leet_1436.java
@@ -1,5 +1,42 @@
 package LeetCode_ADailyTopic;
 
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Leet_1436 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String destCity1(List<List<String>> paths) {
+        Set<String> resSet = new HashSet<>();
+        for (List<String> path : paths) {
+            resSet.add(path.get(0));
+        }
+        for (List<String> path : paths) {
+            String s = path.get(1);
+            if (!resSet.contains(s)) {
+                return s;
+            }
+
+        }
+        return null;
+    }
+
+    public String destCity2(List<List<String>> paths) {
+        StringBuilder sb = new StringBuilder();
+        for (List<String> path : paths) {
+            sb.append(path.get(0));
+        }
+        for (List<String> path : paths) {
+            String s = path.get(1);
+            if (sb.indexOf(s) == -1) {
+                return s;
+            }
+        }
+        return null;
+    }
 }
