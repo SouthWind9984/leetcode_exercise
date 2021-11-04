d1bea61a369515e89385c9bbf86da1ee88587de1
SouthWind <1924151661@qq.com>
2021-11-04 22:31:40 +0800

    LeetCode 每日一题

--- Leet_367.java
+++ Leet_367.java
@@ -3,9 +3,33 @@ public class Leet_367 {
 
     }
 
+    /*
+    1 + 3 + 5 + (2*n-1)  = n^2 --> 前n项奇数之和
+     */
     public boolean isPerfectSquare(int num) {
+        int num1 = 1;
+        while(num > 0)
+        {
+            num -= num1;
+            num1 += 2;
+        }
+        return num == 0;
+    }
 
-
+    public boolean dichotomy(int num){
+        int l = 1;
+        int r = num;
+        while (l<r){
+            int mid = l + (r - l)/2;
+            int tmp = num / mid;
+            if (tmp<=mid){
+                if (num==tmp && num%mid==0) return true;
+                l = mid + 1;
+            }
+            else{
+                r = mid - 1;
+            }
+        }
         return false;
     }
 }
